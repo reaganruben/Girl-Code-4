965a0a21b2cb9e65d7c9373e0a862613ed0cda3f
Reagan Ruben <reaganaisulu@gmail.com>
2022-08-02 15:26:39 -0400

    Added files

---
+++ Girl Code/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  Girl Code
+//
+//  Created by Reagan Ruben on 8/2/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
