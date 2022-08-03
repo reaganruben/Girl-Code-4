a3a9c6763378dcf57d82b003c3dcef24b32392b7
Reagan Ruben <reaganaisulu@gmail.com>
2022-08-03 13:47:40 -0400

    Added login and sign-up pages

---
+++ Girl Code/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  Girl Code
+//
+//  Created by Reagan Ruben on 8/3/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton)
+    {
+    }
+    
+    @IBAction func signUpPressed(_ sender: UIButton)
+    {
+        
+    }
+}
