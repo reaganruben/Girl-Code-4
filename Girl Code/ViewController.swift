bf57f0c60b9938c75f58ac418a048aaa89f6c7b9
Reagan Ruben <reaganaisulu@gmail.com>
2022-08-04 16:07:46 -0400

    added arrays for Steminist names and descriptions

--- Girl Code/ViewController.swift
+++ Girl Code/ViewController.swift
@@ -10,13 +10,36 @@ import UIKit
 class ViewController: UIViewController
 {
     @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var mainScript: UILabel!
+
+    @IBOutlet weak var steministLabel: UILabel!
+    @IBOutlet weak var steministName: UILabel!
+    var steministNamesArray = ["name1", "name2", "name3", "name4", "name5"]
+    @IBOutlet weak var steministScript: UILabel!
+    var steministScriptArray = ["script1", "script2", "script3", "script4", "script5"]
+    
     
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
     }
-
+    var counter = 0
+    @IBAction func generateSteminist(_ sender: UIButton)
+    {
+        if(counter < steministNamesArray.count)
+        {
+            steministName.text = steministNamesArray[counter]
+            steministScript.text = steministScriptArray[counter]
+            counter += 1
+        }
+        else
+        {
+            counter = 0
+            steministName.text = steministNamesArray[counter]
+            steministScript.text = steministScriptArray[counter]
+            counter += 1
+        }
+    }
     
-
 }
