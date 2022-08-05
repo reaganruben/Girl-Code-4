80d34d940fec1145d970820662a1edee2f095d86
Jessica Esch <golferjess428@gmail.com>
2022-08-04 22:37:20 -0400

    finished the buttons on the opportunities page

--- Girl Code/ThirdViewController.swift
+++ Girl Code/ThirdViewController.swift
@@ -21,6 +21,33 @@ class ThirdViewController: UIViewController {
         UIApplication.shared.open(url)
     }
     
+    
+    @IBAction func stemInternships(_ sender: UIButton) {
+        let url = URL (string:
+        "https://docs.google.com/spreadsheets/d/1M8S9MaCwHuUZSFaZ5VwFne3ZWWgqq0jy1-B070aXhGI/edit#gid=0")!
+        UIApplication.shared.open(url)
+    }
+    
+    
+    @IBAction func allPrograms(_ sender: UIButton) {
+        let url = URL (string:
+        "https://docs.google.com/spreadsheets/d/1FpU3mJroN2S-u5GUdWotB7t9eUOI2dUalf_xdAYLPIk/edit#gid=0")!
+        UIApplication.shared.open(url)
+    }
+    
+    
+    @IBAction func stemPrograms(_ sender: UIButton) {
+        let url = URL (string:
+        "https://docs.google.com/spreadsheets/d/1IPSgGa3VXzocBchDZfIG5v-oMNMsiR4gJcsKJ3sC5P0/edit#gid=0")!
+        UIApplication.shared.open(url)
+    
+    }
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
